a56393cd9f7f7e73d7af273e72d89ce3f0f175f5
sdavi222 <130076128+sdavi222@users.noreply.github.com>
2023-09-24 11:48:26 -0400

    Added config folder

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+// establish connection to MongoDB
+const { connect, connection } = require('mongoose');
+// require("dotenv").config()
+const connectionString =
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB';
+
+connect(connectionString)
+
+module.exports = connection;
